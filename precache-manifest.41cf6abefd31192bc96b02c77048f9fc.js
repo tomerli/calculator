7b197d5299e541c9fa2b15ec0697ea29d2d31f9e
Tomer <tomerli@gmail.com>
2024-11-28 10:02:54 +0200

    Updates

---
+++ precache-manifest.41cf6abefd31192bc96b02c77048f9fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "118b56d8fdeacd5d439898f45240a043",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "9b92d034df4db429be6e",
+    "url": "/calculator/static/css/main.09ee2f94.chunk.css"
+  },
+  {
+    "revision": "28d6df99349546cf2cf0",
+    "url": "/calculator/static/js/2.c045c614.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/calculator/static/js/2.c045c614.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b92d034df4db429be6e",
+    "url": "/calculator/static/js/main.0248e709.chunk.js"
+  },
+  {
+    "revision": "e765e492bb7ea77de3f8",
+    "url": "/calculator/static/js/runtime-main.0945a1b5.js"
+  }
+]);
